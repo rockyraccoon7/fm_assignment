4874af907425b90f7848762710f59e7894448772
rockyraccoon7 <Deepak.Karamchandani2@cognizant.com>
2024-04-30 17:16:34 +0530

    second commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Vendor(models.Model):
+    name = models.CharField(max_length=255)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(max_length=50, unique=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=50, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.po_number
+
+class HistoricalPerformance(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"{self.vendor.name} - {self.date}"
