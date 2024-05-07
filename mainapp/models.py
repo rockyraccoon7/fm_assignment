c80d06f47804237a14f9fa762ded95eeef986d68
rockyraccoon7 <168543008+rockyraccoon7@users.noreply.github.com>
2024-05-07 14:41:37 +0530

    functions added

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .views import calculate_on_time_delivery_rate
 
 
 
--- mainapp/signals.py
+++ mainapp/signals.py
@@ -7,35 +7,36 @@ def calculate_delivery_rate(vendor):
     completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status='completed')
     count_completed_orders = completed_orders.count()
     if count_completed_orders == 0:
-        return 0
+        on_time_delivery_rate = 0
     on_time_orders = completed_orders.filter(delivery_date__lte=timezone.now())
     on_time_delivery_rate = (on_time_orders.count() / count_completed_orders) * 100
-    return on_time_delivery_rate
+    vendor.on_time_delivery_rate = on_time_delivery_rate
+    vendor.save()
 
 def average_quality_rating(vendor):
     completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status='completed', quality_rating!=None)
     count_completed_orders = completed_orders.count()
-    total_rq = 0
+    total_qr, aqr = 0, 0
     if not count_completed_orders == 0:
         for i in completed_orders:
             total_qr += i.quality_rating
-        return total_rq/count_completed_orders
-    return 0
+        aqr = total_qr/count_completed_orders
+    vendor.quality_rating_avg = aqr
+    vendor.save()
+
+def average_response_time(vendor):
+    all_orders = PurchaseOrder.objects.filter(vendor=vendor, issue_date != None, acknowledgment_date != None)
+    total_rt = 0
+    for i in all_orders:
+        
 
 
 @receiver(post_save, sender=PurchaseOrder)
 def calculate_on_time_delivery_rate(sender, instance, created):
     if not created and instance.status == 'completed':
-        vendor = instance.vendor
-        new_delivery_rate = calculate_delivery_rate(vendor)
-        vendor.on_time_delivery_rate = new_delivery_rate
-        vendor.save()
-
-@receiver(post_save, sender=PurchaseOrder)
-def calculate_average_quality_rating(sender, instance, created):
+        calculate_delivery_rate(instance.vendor)
     if not created and instance.status == 'completed' and instance.quality_rating != None:
-        average_rq = average_quality_rating(instance.vendor)
-        instance.vendor.quality_rating_avg = average_rq
-        instance.vendor.save()
+        average_quality_rating(instance.vendor)
+    if instance.acknowledged_data is not None:
 
 
